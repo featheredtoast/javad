14f41501ac665484602949b7c444b1565699be7c
Jeff Wong <awole20@gmail.com>
2016-04-07 13:49:04 -0700

    move to gradle

--- java6_jar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ant clean
-export JAVA6_BOOTCLASSES=""
-for i in /usr/lib/jvm/java-6-openjdk-amd64/jre/lib/*.jar; do
-    export JAVA6_BOOTCLASSES=$JAVA6_BOOTCLASSES:$i
-done
-echo $JAVA6_BOOTCLASSES
-ant jar
